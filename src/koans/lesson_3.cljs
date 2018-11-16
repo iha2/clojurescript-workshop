f70d9bbba4499e082d0998bfedc1ecd4fc994e48
Iha2 <iheatudeveloper@gmail.com>
2018-11-16 17:10:00 -0500

    Map koans finished

--- src/koans/lesson_3.cljs
+++ src/koans/lesson_3.cljs
@@ -1,51 +1,45 @@
 (ns koans.lesson_3)
 
+;; Clojurescript comes with hash maps as it's basic data structures
 
-;; Clojurescript comes with very basic data structures
+;; You can create a with the 'hash-map' function
+(= {:a 1 :b 2} (:__ :a 1 :b 2))
 
-;; 
-(= {:a 1 :b 2} (hash-map :a 1 :__))
+;; But a value must be supplied for each key
+(= {:a 1} (hash-map :a 1))
 
-"A value must be supplied for each key"
-(= {:a 1} (hash-map :a :__))
-
-"The size is the number of entries"
+;; The size is the number of entries
 (= :__ (count {:a 1 :b 2}))
 
-"You can look up the value for a given key"
+;; You can look up the value for a given key
 (= :__ (get {:a 1 :b 2} :b))
 
-"Maps can be used as functions to do lookups"
+;; However Maps can also act like function (since they essentially are right?)
 (= :__ ({:a 1 :b 2} :a))
 
-"And so can keywords"
+;; Keywords can also act like functions
 (= :__ (:a {:a 1 :b 2}))
 
-"But map keys need not be keywords"
+;; But map keys need not be keywords
 (= :__ ({2006 "Torino" 2010 "Vancouver" 2014 "Sochi"} 2010))
 
-"You may not be able to find an entry for a key"
-(= :__ (get {:a 1 :b 2} :c))
+;; You may not be able to find an entry for a key
+(= :__ ({:a 1 :b 2} :c))
 
 "But you can provide your own default"
-(= :__ (get {:a 1 :b 2} :c :key-not-found))
+(= :__ ({:a 1 :b 2} :c :key-not-found))
 
-"You can find out if a key is present"
-(= :__ (contains? {:a nil :b nil} :b))
+;; You can find out if a key is present with 'contains?'
+(= :__ (:__ {:a nil :b nil} :b))
 
-"Or if it is missing"
-(= :__ (contains? {:a nil :b nil} :c))
+;; Maps are immutable, but you can create an new version with 'assoc'
+(= {1 "January" 2 :__} (:__ {1 "January"} 2 "February"))
 
-"Maps are immutable, but you can create a new and improved version"
-(= {1 "January" 2 :__} (assoc {1 "January"} 2 "February"))
+;; You can also create a new version with an entry removed with 'dissoc'
+(= :__ (:__ {1 "January" 2 "February"} 2))
 
-"You can also create a new version with an entry removed"
-(= :__ (dissoc {1 "January" 2 "February"} 2))
+;; Often you will need to get the 'keys', but the order is undependable
+(:__ {2010 "Vancouver" 2014 "Sochi" 2006 "Torino"})
 
-"Often you will need to get the keys, but the order is undependable"
-(= (list :__)
-   (sort (keys {2010 "Vancouver" 2014 "Sochi" 2006 "Torino"})))
-
-"You can get the values in a similar way"
-(= (list :__)
-   (sort (vals {2006 "Torino" 2010 "Vancouver" 2014 "Sochi"})))
+;; And you can get the 'vals' as well
+(:__ {2006 "Torino" 2010 "Vancouver" 2014 "Sochi"})
