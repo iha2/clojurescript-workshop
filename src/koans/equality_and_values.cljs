71d194658614d0d3d89d44775f6325f0bd08e5dc
Iha2 <iheatudeveloper@gmail.com>
2018-11-16 14:21:51 -0500

    answers to the koans

--- src/koans/equality_and_values.cljs
+++ src/koans/equality_and_values.cljs
@@ -1,37 +1,37 @@
 (ns koans.values)
 
-(= _ true)
+(= true true)
 
 ;; quality is simply another variadic function
-(= (+ 3 4) 7 (+ _ 5))
+(= (+ 3 4) 7 (+ 2 5))
 
 ;; = is the same as == in javascript
-(= _ (= 2 2.0))
+(= true (= 2 2.0))
 
 ;; But it does not have the loose equality of javascript
-(= _ (= "2" 2.0))
+(= false (= "2" 2.0))
 
-;; nil is null and undefined
-(= (= true nil) _)
+;; nil is null and undefined in one beatiful bunch
+(= (= true nil) false)
 
 ;; however, nil is not truthy
-(= (= false nil) _)
+(= (= false nil) false)
 
 
 
 ;; --- Strings, Keywords, Symbols" --- ;;
 
 ;; However they are not the same thing
-(= _ (= "hello" :hello 'hello))
+(= false (= "hello" :hello 'hello))
 
 ;;  Make a keyword with your keyboard
-(= _ (keyword "hello"))
+(= :hello (keyword "hello"))
 
 ;; Symbolism is all around us
-(= _ (symbol "hello"))
+(= 'hello (symbol "hello"))
 
 ;; What could be equivalent to nothing? (like a real language)
-(= _ nil)
+(= nil nil)
 
 ;; When things cannot be equal, they must be different
 (not= :fill-in-the-blank :aaa)
