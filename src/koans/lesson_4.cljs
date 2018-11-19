d3baec38944a588e9093f035068e7da1a75edac3
Iha2 <iheatudeveloper@gmail.com>
2018-11-19 12:43:18 -0500

    answers

--- src/koans/lesson_4.cljs
+++ src/koans/lesson_4.cljs
@@ -6,49 +6,46 @@
 ;; STORING VARIABLES
 
 ;; creating variables is done using 'def'. Enter a name for your variable
-(:__ :__ {:a 1 :b 2})
+(def object-container {:a 1 :b 2})
 
 
 ;; Run an anonymous function
 (#(+ %1 %2) 3 4)
 
 ;; you can store a function with def
-(def my-function #(+ %:__ %:__))
+(def my-function #(+ %1 %2))
 
 ;; thankfully clojurescript comes with a macro for storing functions called 'defn'
 (defn new-function [x y] (+ x y))
-
 
 ;; Recursion: Loop & Recur
 
 ;; This is an example of a for loop using the loop and recur
 ;; For loop
 (defn for-loop [function count]
-  (loop [iterator loop-state]
-    (if (= loop-state 0)
+  (loop [iterator count]
+    (if (= iterator 0)
       nil
-      (recur (dec loop-state)))))
+      (do
+        (function)
+        (recur (dec iterator))))))
 
 ;; impliment a map higher order function. Use the functions 'first', 'rest' and 'empty?'
-  
+
 (defn !map [lamda-expression map-collection]
   (loop [collection map-collection
          result '()]
-    (if (:__ collection)
-      :__
-      (recur :__ :__)
-      )))
+    (if (empty? collection)
+      result
+      (recur (rest collection) (conj result (lamda-expression (first collection)))))))
 
 ;; impliment a filter higher order function. Use the functions 'first', 'rest' and 'empty?'
-
 (defn !filter [lambda-expression map-collection]
   (loop [collection map-collection
          result '()]
-    (let [lambda-result :__]
-      (cond (:__ collection) result
-            lambda-result (recur :__ :__)
-            :else (recur :__ :__)))
-    ))
-
+    (let [lambda-result (lambda-expression (first collection))]
+      (cond (empty? collection) result
+            lambda-result (recur (rest collection) (conj result (first collection)))
+            :else (recur (rest collection) result)))))
 
 ;; implement reduce using  the loop and recur, along with any other functions used in the previous exercises.
